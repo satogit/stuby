9e9e8b6e53e3e87a9c7dbc703085080942c9c4fd
さと <notfound.pan@gmail.com>
2015-03-20 22:29:25 +0900

    サーブレットサンプル追加

---
+++ src/web01/Servlet01.java
@@ -0,0 +1,26 @@
+package web01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Servlet01() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getOutputStream().write("hello".getBytes());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
